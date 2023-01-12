3f95e4d31a332ed1adce77ba6d034b73c992f5b1
Kristan Ruiz <itan_07@hotmail.com>
2023-01-12 14:43:01 -0700

    (text) Add introduction text to user

--- Program.cs
+++ Program.cs
@@ -2,11 +2,24 @@
 
 namespace calculator
 {
-  internal class program
+  internal class roleplay
   {
     static void Main(string[] args)
     {
+      roleplay.introduction();
 
+
+    }
+
+    static void introduction()
+    {
+      Console.WriteLine("---Role Play!---");
+
+      Console.WriteLine("Hi!. Have you ever dream with a role-play generator without any GUI (Graphic User Interface)?, me neither.");
+
+      Console.WriteLine("\nRight now you're inside a fancy fantasy world and you're about to born in a new magic adventure.");
+
+      Console.WriteLine("\tChoose what kind of character would you like to be in your journey:");
     }
   }
 }
